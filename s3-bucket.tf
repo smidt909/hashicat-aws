96bfc2e677838f38c2d160bc33178f955264f647
smidt909 <robert_smidt@fanniemae.com>
2022-08-30 19:19:31 +0000

    Added s3 bucket module correction

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,13 +1,6 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
+module "s3-bucket" {
+  source  = "app.terraform.io/s4urgsTTraining/s3-bucket/aws"
+  version = "2.8.0"
   bucket_prefix = "${var.prefix}"
-
-  versioning = {
-    enabled = true
-  }
 
 }
